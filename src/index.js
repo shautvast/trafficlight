f97f56f837d663419f13ced4fc4c6fcd7b6e3467
Sander Hautvast <shautvast@gmail.com>
2020-01-17 15:56:28 +0100

    fix remove password

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-const express = require('express')
+const express = require('express');
 const fs = require('fs');
-const bodyParser = require('body-parser')
-const path = require('path')
-var bcrypt = require('bcrypt');
-const PORT = process.env.PORT || 5000
-
-const logins = new Map();
-logins.set("sander", "$2b$10$6P.6pE7M/6C9l/xXKDxJFucTL313GwESnhZ3aAqtVnv.ouLca/y6a");
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.urlencoded({ extended: true }))
+    .use(express.urlencoded({extended: true}))
     .use(express.static(path.join(__dirname, '/public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
@@ -19,23 +15,17 @@ express()
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function appendToStorage(req, res) {
-    if (req.body.username === '' || req.body.password === '') {
+    if (req.body.username === '') {
         res.render('pages/error');
     } else {
-        if (!correctCredentials(req.body.username, req.body.password)) {
-            res.render('pages/error');
-        } else {
-            fs.appendFile("dat", req.body.timestamp + ", " + req.body.username + ", " + req.body.emotion + "\n", err => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-            res.render('pages/thanks');
-        }
+        fs.appendFile("dat", req.body.timestamp + ", " + req.body.username + ", " + req.body.emotion + "\n", err => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        res.render('pages/thanks');
     }
 }
-
-
 
 function sendData(req, res) {
     fs.readFile("dat", (err, data) => {
@@ -46,8 +36,4 @@ function sendData(req, res) {
             return res.send(data);
         }
     });
-}
-
-function correctCredentials(username, password) {
-    return bcrypt.compareSync(password, logins.get(username));
 }
